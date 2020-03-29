b651855faf67836bdae760f8c8c41bdc0b4d64f4
myatniy <ipsixin3d@gmail.com>
2020-03-29 22:11:27 +0300

    Change tabs to spaces

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -1,6 +1,6 @@
 const process = require('process');
 const fs = require('fs');
-const {program} = require('commander');
+const { program } = require('commander');
 const caesar = require('crypto-classic-caesar');
 const chalk = require('chalk');
 
@@ -8,70 +8,76 @@ const error = console.error;
 const log = console.log;
 
 program
-	.option('-s, --shift <shift>', 'a shift')
-	.option('-a, --action <action>', 'an action encode/decode')
-	.option('-i, --input <fileInput>', 'an input file')
-	.option('-o, --output <fileOutput>', 'an output file')
-	.parse(process.argv);
+  .option('-s, --shift <shift>', 'a shift')
+  .option('-a, --action <action>', 'an action encode/decode')
+  .option('-i, --input <fileInput>', 'an input file')
+  .option('-o, --output <fileOutput>', 'an output file')
+  .parse(process.argv);
 
 let shift = parseInt(program.shift);
 let action = program.action;
 
-if(Number.isInteger(shift) && shift !== 0) {
-	log(`Shift's value is: ${chalk.bgCyanBright.black(shift)}`);
+if (Number.isInteger(shift) && shift !== 0) {
+  log(`Shift's value is: ${chalk.bgCyanBright.black(shift)}`);
 } else {
-	throw (`-s, --shift required and must be a number and not to be equal 0.\nYour shift's value is: ${chalk.bold.bgYellowBright.red(program.shift)}`);
+  throw `-s, --shift required and must be a number and not to be equal 0.\nYour shift's value is: ${chalk.bold.bgYellowBright.red(
+    program.shift
+  )}`;
 }
 
-if(action == 'encode' || action == 'decode') {
-	log(`Action's value is: ${chalk.bgCyanBright.black(action)}`);
+if (action == 'encode' || action == 'decode') {
+  log(`Action's value is: ${chalk.bgCyanBright.black(action)}`);
 } else {
-	throw (`-a, --action required and takes only encode/decode value.\nYour action's value is: ${chalk.bold.bgYellowBright.red(action)}`);
+  throw `-a, --action required and takes only encode/decode value.\nYour action's value is: ${chalk.bold.bgYellowBright.red(
+    action
+  )}`;
 }
 
 doTheThang(program.output, program.input);
 
 function doTheThang(savPath, srcPath) {
-	fs.readFile(srcPath, function (err, data) {
-		if (err) {
-			error(`Can't find "${chalk.bold.bgYellowBright.red(program.input)}" file.`);
-			
-			process.stdin.setEncoding('utf8');
-			
-			process.stdin.on('readable', () => {
-				var input = process.stdin.read();
-				if(input !== null) {
-					process.stdout.write(`stdout: ${input}`);
+  fs.readFile(srcPath, function(err, data) {
+    if (err) {
+      error(
+        `Can't find "${chalk.bold.bgYellowBright.red(program.input)}" file.`
+      );
 
-					var command = input.trim();
-					
-					data = toEncodeOrDecode(command, action, shift);
+      process.stdin.setEncoding('utf8');
 
-					if(command === 'quit') {
-						process.exit(0);
-					}
+      process.stdin.on('readable', () => {
+        var input = process.stdin.read();
+        if (input !== null) {
+          process.stdout.write(`stdout: ${input}`);
 
-					fs.writeFile (savPath, data, function(err) {
-						if (err) {
-							error(`Can't write to "${program.output}" file`);
-							process.stdout.write(`${action}d text: ${data}\n`);
-						}
-						if (data) log(getActionStatus(action));
-				});
-				}
-			});
-		}
+          var command = input.trim();
 
-		if (data)  data = toEncodeOrDecode(data.toString('utf8'), action, shift);
+          data = toEncodeOrDecode(command, action, shift);
 
-		fs.writeFile (savPath, data, function(err) {
-				if (err) {
-					error(`Can't write to "${program.output}" file`);
-					process.stdout.write(`${action}d text: ${data}\n`);
-				}
-				if (data) log(getActionStatus(action));
-		});
-	});
+          if (command === 'quit') {
+            process.exit(0);
+          }
+
+          fs.writeFile(savPath, data, function(err) {
+            if (err) {
+              error(`Can't write to "${program.output}" file`);
+              process.stdout.write(`${action}d text: ${data}\n`);
+            }
+            if (data) log(getActionStatus(action));
+          });
+        }
+      });
+    }
+
+    if (data) data = toEncodeOrDecode(data.toString('utf8'), action, shift);
+
+    fs.writeFile(savPath, data, function(err) {
+      if (err) {
+        error(`Can't write to "${program.output}" file`);
+        process.stdout.write(`${action}d text: ${data}\n`);
+      }
+      if (data) log(getActionStatus(action));
+    });
+  });
 }
 
 function getActionStatus(action) {
@@ -84,6 +90,6 @@ function getActionStatus(action) {
 
 function toEncodeOrDecode(data, action, shift) {
   return action === 'encode'
-    ? data = caesar.encipher(data, shift)
-    : data = caesar.decipher(data, shift);
+    ? (data = caesar.encipher(data, shift))
+    : (data = caesar.decipher(data, shift));
 }
